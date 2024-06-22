543261d9998638976b9b0825d4f915d2cdc9049c
Zainab Jafari <zainabjafari998@gmail.com>
2024-06-22 19:25:01 +0200

    schema added

--- app/(dashboard)/_components/CreateTransactionDialog.tsx
+++ app/(dashboard)/_components/CreateTransactionDialog.tsx
@@ -1,8 +1,46 @@
-"use client"
+"use client";
 
-import { Dialog, DialogTitle } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { TransactionType } from "@/lib/types";
-import { ReactNode } from "react"
+import { cn } from "@/lib/utils";
+import {
+  CreateTransactionSchema,
+  CreateTransactionSchemaType,
+} from "@/schema/transaction";
+import { ReactNode, useCallback, useState } from "react";
+
+import React from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
+import { format } from "date-fns";
+import { CalendarIcon, Loader2 } from "lucide-react";
+import { Calendar } from "@/components/ui/calendar";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
 
 interface Props {
     trigger: ReactNode;
@@ -10,9 +48,65 @@ interface Props {
 }
 
 function CreateTransactionDialog({trigger, type}: Props){
+    const form = useForm<CreateTransactionSchemaType>({
+        resolver: zodResolver(CreateTransactionSchema),
+        defaultValues: {
+            type,
+            data: new Date(),
+        },
+    })
     return (
         <Dialog>
-            <DialogTitle asChild>{trigger}</DialogTitle>
+            <DialogTrigger asChild>{trigger}</DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+            <DialogTitle>
+                Create a new {' '}
+                <span className={cn(
+                    "m-1",
+                    type === "income" ? "text-emerald-500" :
+                    "text-red-500"
+                )}>
+                    {type}
+                </span>
+                transaction
+            </DialogTitle>
+                </DialogHeader>
+                <Form {...form}>
+                    <form className="space-y-4">
+                        <FormField 
+                        control={form.control}
+                        name="description"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>
+                                    <FormControl>
+                                        <Input defaultValue={""} {...field}/>
+                                    </FormControl>
+                                    <FormDescription>
+                                        Transaction description (optional)
+                                    </FormDescription>
+                                </FormLabel>
+                            </FormItem>
+                        )}/>
+                           <FormField 
+                        control={form.control}
+                        name="amount"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>
+                                    <FormControl>
+                                        <Input defaultValue={0} type="number" {...field}/>
+                                    </FormControl>
+                                    <FormDescription>
+                                        Transaction amount (required)
+                                    </FormDescription>
+                                </FormLabel>
+                            </FormItem>
+                        )}/>
+                    </form>
+                </Form>
+            </DialogContent>
         </Dialog>
     )
 }
